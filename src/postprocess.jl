a574fbe9842f391e1a40f5a9d94a86d46ae51e69
Laura Merchant <lmerchant93@gmail.com>
2016-08-06 14:03:30 +0100

    Hinge_Moment code

--- src/postprocess.jl
+++ src/postprocess.jl
@@ -116,6 +116,39 @@ function calc_forces(surf::TwoDSurfwFlap)
         # Same term as before with additional squared spatial derivative term
         nonl_m = nonl_m + surf.x[in]*sqrt(1+surf.cam_slope[ib]*surf.cam_slope[ib])*(surf.uind[ib]*cos(surf.kinem.alpha) - surf.wind[ib]*sin(surf.kinem.alpha))*surf.x[ib]*surf.bv[ib].s
     end
+    
+    # -------------------------------------------------------------
+    xb = floor(surf.x_b[1]*ndiv)+1
+    m_be1a = 0
+    m_be1b = 0
+    m_be2a = 0
+    m_be2b = 0
+    me_be3 = 0
+    # These are the expressions multipled by the square root term in eqn in Hinge_Moment notebook. 
+    for ib = xb:surf.ndiv-1
+        m_be1a = m_be1a + sqrt(1+surf.cam_slope[ib]*surf.cam_slope[ib])*(surf.kinem.u*cos(surf.kinem.alpha)+surf.kinem.hdot*sin(surf.kinem.alpha) - surf.kinem.alphadot*surf.cam[ib])*surf.bv[ib].s 
+        m_be1b = m_be1b + sqrt(1+surf.cam_slope[ib]*surf.cam_slope[ib])*(surf.uind[ib]*cos(surf.kinem.alpha) - surf.wind[ib]*sin(surf.kinem.alpha))*surf.bv[ib].s
+        m_be2a = m_be2a + surf.x[ib]*sqrt(1+surf.cam_slope[ib]*surf.cam_slope[ib])*(surf.kinem.u*cos(surf.kinem.alpha)+surf.kinem.hdot*sin(surf.kinem.alpha) - surf.kinem.alphadot*surf.cam[ib])*surf.x[ib]surf.bv[ib].s
+        m_be2b = m_be2b + surf.x[ib]*sqrt(1+surf.cam_slope[ib]*surf.cam_slope[ib])*(surf.uind[ib]*cos(surf.kinem.alpha) - surf.wind[ib]*sin(surf.kinem.alpha))*surf.x[ib]*surf.bv[ib].s
+    end 
+    # This block of code relates to the time derivate, double integration term. 
+    hinge = (surf.theta[surf.ndiv] - surf.theta[xb]) # This relates to the segment of the aerofoil being looked over i.e. x_b to x but in terms of theta
+    gam = zeros(surf.ndiv)
+        ib = 1:surf.ndiv
+            gam[ib] = (surf.a0[1]*(1 + cos(surf.theta[ib])))*(surf.x_b[1]-surf.x[ib])*sin(surf.theta[ib])
+            for ia = 1:surf.naterm
+                gam[ib] = gam[ib] + surf.aterm[ia]*sin(ia*surf.theta[ib])*sin(surf.theta[ib])*sin(surf.theta[inb])
+            end
+            gam[ib] = gam[ib]*surf.uref*surf.c*surf.c/2.
+        end
+ 
+    m_be3 = ForwardDiff.derivative(trapz(trapz(gam, surf.theta),hinge),t)
+
+    m_be = surf.x_b[1]*(m_be1a + m_be1b) - (m_be2a + m_be2b) + m_be3 
+
+    cm_be = m_be*2./(surf.uref*surf.uref*surf.c*surf.c)
+    # ---------------------------------------------------------------------------------
+
     nonl = nonl*2./(surf.uref*surf.uref*surf.c)
     nonl_cnc = nonl_cnc*2./(surf.uref*surf.uref*surf.c)
     nonl_m = nonl_m*2./(surf.uref*surf.uref*surf.c*surf.c)
@@ -130,7 +163,7 @@ function calc_forces(surf::TwoDSurfwFlap)
 
     #Pitching moment is clockwise or nose up positive
     cm = cn*surf.pvt - 2*pi*((surf.c/surf.uref)*(7.*surf.a0dot[1]/16. + 3.*surf.adot[1]/16. + surf.adot[2]/16. - surf.adot[3]/64.)) - nonl_m1 - nonl_m
-    return cl, cd, cm
+    return cl, cd, cm, cm_be
 end
 # ---------------------------------------------------------------------------------------------
 

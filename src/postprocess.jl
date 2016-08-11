7d8cb5172ca7a2c2f3df2b18f2218bb3c1e056e0
Laura Merchant <lmerchant93@gmail.com>
2016-08-11 17:28:32 +0100

    View_Vorts
    Updated functions to produce plots based on vortex strength.

--- src/postprocess.jl
+++ src/postprocess.jl
@@ -1,22 +1,26 @@
 # ---------------------------------------------------------------------------------------------
 # Function for viewing the 2D vorticity field around the airfoil
 function view_vorts(surf::TwoDSurf, field::TwoDFlowField)
-    scatter(map(q->q.x, field.tev),map(q->q.z,field.tev))
-    scatter(map(q->q.x, field.lev),map(q->q.z,field.lev))
-    plot(map(q->q.x, surf.bv),map(q->q.z,surf.bv))
+    scatter(map(q->q.x, field.tev),map(q->q.z,field.tev),s=20,c=map(q->q.s,field.tev),cmap=ColorMap("jet"),edgecolors="none")
+    sc = scatter(map(q->q.x, field.lev),map(q->q.z,field.lev),s=20,c=map(q->q.s,field.lev),cmap=ColorMap("jet"),edgecolors="none")
+    plot(map(q->q.x, surf.bv),map(q->q.z,surf.bv),color = "black",linewidth=2.0)
+colorbar(sc)
 end
 
 function view_vorts(surf::TwoDSurf_2DOF, field::TwoDFlowField)
-    scatter(map(q->q.x, field.tev),map(q->q.z,field.tev))
-    scatter(map(q->q.x, field.lev),map(q->q.z,field.lev))
-    plot(map(q->q.x, surf.bv),map(q->q.z,surf.bv))
+     scatter(map(q->q.x, field.tev),map(q->q.z,field.tev),s=20,c=map(q->q.z,field.tev),cmap=ColorMap("jet"))
+    sc = scatter(map(q->q.x, field.lev),map(q->q.z,field.lev),s=20,c=map(q->q.z,field.lev),cmap=ColorMap("jet"))
+    plot(map(q->q.x, surf.bv),map(q->q.z,surf.bv),color = "black",linewidth=1.5)
+colorbar(sc)
 end
 
 function view_vorts(surf::TwoDSurfwFlap, field::TwoDFlowField)
-    scatter(map(q->q.x, field.tev),map(q->q.z,field.tev))
-    scatter(map(q->q.x, field.lev),map(q->q.z,field.lev))
-    plot(map(q->q.x, surf.bv),map(q->q.z,surf.bv))
+     scatter(map(q->q.x, field.tev),map(q->q.z,field.tev),s=20,c=map(q->q.s,field.tev),cmap=ColorMap("jet"),edgecolors="none")
+    sc = scatter(map(q->q.x, field.lev),map(q->q.z,field.lev),s=20,c=map(q->q.z,field.lev),cmap=ColorMap("jet"),edgecolors="none")
+    plot(map(q->q.x, surf.bv),map(q->q.z,surf.bv),color = "black",linewidth=2.0)
+colorbar(sc)
 end
+
 
 # ---------------------------------------------------------------------------------------------
 # Function that calculates the aerodynamic forces

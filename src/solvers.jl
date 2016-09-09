cc6afcddc7a20304d7fd7c18bec96dced3d433a8
KiranUofG <Kiran.Ramesh@glasgow.ac.uk>
2016-09-09 13:32:43 -0300

    2dOF

--- src/solvers.jl
+++ src/solvers.jl
@@ -122,11 +122,11 @@ function theodorsen(theo::TheoDef)
     # alpha_mean = mean angle of attack of chord line
     # alpha_zl = zero-lift angle of attack of airfoil
     # reduced frequency
-    
+
     # Motion definitions
     # h = h_amp*exp(i*wt)
     # alpha = alpha_mean + alpha_amp*exp(i*(wt + phi))
-    
+
     #define a
     a = (theo.pvt-0.5)/0.5
 
@@ -134,21 +134,21 @@ function theodorsen(theo::TheoDef)
 
     #Theodorsen function
     C = besselh(1,2,theo.k)./(besselh(1,2,theo.k) + im*besselh(0,2,theo.k))
-    
+
     # steady-state Cl
     Cl_ss = 2*pi*(theo.alpha_mean - theo.alpha_zl)
-    
+
     # plunge contribution
     Cl_h = 2*pi*theo.k^2*theo.h_amp*exp(im*wt) - im*4*pi*theo.k*C*theo.h_amp*exp(im*wt)
-    
+
     # pitch contribution
     Cl_alpha = (im*pi*theo.k + pi*theo.k^2*a)*theo.alpha_amp*exp(im*(wt+theo.phi)) + (1 + im*theo.k*(0.5-a))*2*pi*C*theo.alpha_amp*exp(im*(wt+theo.phi))
-    
+
     # total contributions
     Cl_tot = Cl_ss + Cl_h + Cl_alpha
 
     return wt/(2*pi), Cl_h, Cl_alpha, Cl_tot
-    
+
 end
 
 function theodorsen(theo::TheoDefwFlap)
@@ -162,12 +162,12 @@ function theodorsen(theo::TheoDefwFlap)
     # alpha_mean = mean angle of attack of chord line
     # alpha_zl = zero-lift angle of attack of airfoil
     # xf = xf/c, non-dimensional flap location
-    
+
     # Motion definitions
     # h = h_amp*exp(i*wt)
     # alpha = alpha_mean + alpha_amp*exp(i*(wt + phi))
     # beta =  beta_amp*exp(i*(wt + psi))
-    
+
     #define a and c
     a = (theo.pvt-0.5)/0.5
     c = (theo.xf-0.5)/0.5
@@ -175,7 +175,7 @@ function theodorsen(theo::TheoDefwFlap)
     #Define the required coefficients
     T1 = -(2+c*c)*sqrt(1-c*c)/3 + c*acos(c)
     T4 = c*sqrt(1-c*c) - acos(c)
-    T11 = (2-c)*sqrt(1-c*c) + (1-2*c)*acos(c)  
+    T11 = (2-c)*sqrt(1-c*c) + (1-2*c)*acos(c)
     T12 = (2+c)*sqrt(1-c*c) + (1-2*c)*acos(c)
     T2 = T4*(T11+T12)
     T3 = -(1-c*c)*(5*c*c+4)/8 + c*(7+2*c*c)*sqrt(1-c*c)*acos(c)/4 - (1/8+c*c)*acos(c)*acos(c)
@@ -193,24 +193,24 @@ function theodorsen(theo::TheoDefwFlap)
     T18 = T5 - T4*T10
     T19 = T4*T11
     T20 = T10 - 2*sqrt(1-c*c)
-    
+
     wt = [0:2*pi/360:2*pi;]
 
     #Theodorsen function
     C = besselh(1,2,theo.k)./(besselh(1,2,theo.k) + im*besselh(0,2,theo.k))
-    
+
     # steady-state Cl
     Cl_ss = 2*pi*(theo.alpha_mean - theo.alpha_zl)
 
-    #Derived in a Julia notebook (Available in the UNSflow package) 
-    # Lift 
+    #Derived in a Julia notebook (Available in the UNSflow package)
+    # Lift
     Cl_h = 2*pi*theo.h_amp*theo.k*(-2*im*C+theo.k)*exp(im*wt)
     Cl_alpha = -pi*theo.alpha_amp*(2*C*(im*theo.k*(a-0.5)-1) - theo.k*(a*theo.k+im))*exp(im*(theo.phi+wt))
     Cl_beta = theo.beta_amp*(C*(im*theo.k*T11+2*T10) + theo.k*(theo.k*T1-im*T4))*exp(im*(wt+theo.psi))
     # -------------------------------------
     # Pitching moment
     Cmal_h = -pi*theo.h_amp*theo.k*(2*im*C*(a + 0.5) - a*theo.k)*exp(im*wt)
-    Cmal_al = -0.5*pi*theo.alpha_amp*(2*C*(a + 0.5)*(im*theo.k*(a + 1) - 1) - theo.k*(theo.k*(a^2 + 0.125) + im*(a - 0.5)))*exp(im*(theo.phi + wt)) 
+    Cmal_al = -0.5*pi*theo.alpha_amp*(2*C*(a + 0.5)*(im*theo.k*(a + 1) - 1) - theo.k*(theo.k*(a^2 + 0.125) + im*(a - 0.5)))*exp(im*(theo.phi + wt))
     Cmal_be = 0.5*theo.beta_amp*(C*(a+0.5)*(im*theo.k*T11* + 2*T10) + 2*T13*theo.k^2 - im*theo.k*T16 - T15)*exp(im*(wt + theo.psi))
     # -------------------------------------
     # Hinge moment
@@ -232,7 +232,7 @@ end
 
 function ldvm(surf::TwoDSurf, curfield::TwoDFlowField, nsteps::Int64 = 500, dtstar::Float64 = 0.015, delvort = DelVortDef(0, 0, 0.))
     mat = zeros(nsteps,8)
-    
+
     dt = dtstar*surf.c/surf.uref
     t = 0.
 
@@ -246,7 +246,7 @@ function ldvm(surf::TwoDSurf, curfield::TwoDFlowField, nsteps::Int64 = 500, dtst
 
         #Update flow field parameters if any
         update_externalvel(curfield, t)
-        
+
         #Update bound vortex positions
         update_boundpos(surf, dt)
 
@@ -258,7 +258,7 @@ function ldvm(surf::TwoDSurf, curfield::TwoDFlowField, nsteps::Int64 = 500, dtst
         #curfield.tev[length(curfield.tev)].s = secant_method(kelv, 0., -0.01)
         soln = nlsolve(not_in_place(kelv), [-0.01])
         curfield.tev[length(curfield.tev)].s = soln.zero[1]
-        
+
         #Check for LESP condition
         #Update values with converged value of shed tev
         #Update incduced velocities on airfoil
@@ -308,9 +308,9 @@ function ldvm(surf::TwoDSurf, curfield::TwoDFlowField, nsteps::Int64 = 500, dtst
 
         #Calculate bound vortex strengths
         update_bv(surf)
-        
+
         #Remove vortices that are far away from airfoil
-        if (delvort.flag == 1) 
+        if (delvort.flag == 1)
             if length(curfield.tev) > delvort.limit
                 if (sqrt((curfield.tev[1].x- surf.bnd_x[div(surf.ndiv,2)])^2 + (curfield.tev[1].z- surf.bnd_z[div(surf.ndiv,2)])^2) > delvort.dist*surf.c)
                     kelv_enf = kelv_enf + curfield.tev[1].s
@@ -331,13 +331,13 @@ function ldvm(surf::TwoDSurf, curfield::TwoDFlowField, nsteps::Int64 = 500, dtst
             end
         end
         wakeroll(surf, curfield, dt)
-        
+
         cl, cd, cm = calc_forces(surf)
         mat[istep,:] = [t surf.kinem.alpha surf.kinem.h surf.kinem.u surf.a0[1] cl cd cm]
     end
 
 
-    
+
     mat, surf, curfield
     #Plot flowfield viz
 #    figure(0)
@@ -348,7 +348,7 @@ end
 
 function ldvm(surf::TwoDSurfwFlap, curfield::TwoDFlowField, nsteps::Int64 = 500, dtstar::Float64 = 0.015)
     mat = zeros(nsteps,9)
-    
+
     dt = dtstar*surf.c/surf.uref
     t = 0.
 
@@ -362,7 +362,7 @@ function ldvm(surf::TwoDSurfwFlap, curfield::TwoDFlowField, nsteps::Int64 = 500,
 
         #Update deformation
         update_deform(surf, t)
-        
+
         #Update bound vortex positions
         update_boundpos(surf, dt)
 
@@ -374,7 +374,7 @@ function ldvm(surf::TwoDSurfwFlap, curfield::TwoDFlowField, nsteps::Int64 = 500,
         #curfield.tev[length(curfield.tev)].s = secant_method(kelv, 0., -0.01)
         soln = nlsolve(not_in_place(kelv), [-0.01])
         curfield.tev[length(curfield.tev)].s = soln.zero[1]
-        
+
         #Check for LESP condition
         #Update values with converged value of shed tev
         #Update incduced velocities on airfoil
@@ -428,7 +428,7 @@ function ldvm(surf::TwoDSurfwFlap, curfield::TwoDFlowField, nsteps::Int64 = 500,
         wakeroll(surf, curfield, dt)
 
         cl, cd, cm, cm_be = calc_forces(surf, dt)
-        
+
         mat[istep,:] = [t surf.kinem.alpha surf.kinem.h surf.kinem.u surf.a0[1] cl cd cm cm_be]
 
     end
@@ -440,7 +440,7 @@ function ldvm(surf::TwoDSurfwFlap, curfield::TwoDFlowField, nsteps::Int64 = 500,
 
 end
 
-function ldvm(surf::TwoDSurf_2DOF, curfield::TwoDFlowField, nsteps::Int64 = 500, dtstar::Float64 = 0.015)
+function ldvm(surf::TwoDSurf_2DOF, curfield::TwoDFlowField, nsteps::Int64 = 500, dtstar::Float64 = 0.015, delvort = DelVortDef(0, 0, 0.))
     mat = zeros(nsteps,8)
 
     dt = dtstar*surf.c/surf.uref
@@ -453,7 +453,7 @@ function ldvm(surf::TwoDSurf_2DOF, curfield::TwoDFlowField, nsteps::Int64 = 500,
 
         #Update kinematic parameters (based on 2DOF response)
         if (istep > 1) # Allow initial condition
-            update_kinem(surf, dt) 
+            update_kinem(surf, dt)
         end
         #Update bound vortex positions
         update_boundpos(surf, dt)
@@ -466,7 +466,7 @@ function ldvm(surf::TwoDSurf_2DOF, curfield::TwoDFlowField, nsteps::Int64 = 500,
         #curfield.tev[length(curfield.tev)].s = secant_method(kelv, 0., -0.01)
         soln = nlsolve(not_in_place(kelv), [-0.01])
         curfield.tev[length(curfield.tev)].s = soln.zero[1]
-        
+
         #Check for LESP condition
         #Update values with converged value of shed tev
         #Update incduced velocities on airfoil
@@ -517,17 +517,39 @@ function ldvm(surf::TwoDSurf_2DOF, curfield::TwoDFlowField, nsteps::Int64 = 500,
         #Calculate bound vortex strengths
         update_bv(surf)
 
+        #Remove vortices that are far away from airfoil
+        if (delvort.flag == 1)
+            if length(curfield.tev) > delvort.limit
+                if (sqrt((curfield.tev[1].x- surf.bnd_x[div(surf.ndiv,2)])^2 + (curfield.tev[1].z- surf.bnd_z[div(surf.ndiv,2)])^2) > delvort.dist*surf.c)
+                    kelv_enf = kelv_enf + curfield.tev[1].s
+                    for i = 1:length(curfield.tev)-1
+                        curfield.tev[i] = curfield.tev[i+1]
+                    end
+                    pop!(curfield.tev)
+                end
+            end
+            if length(curfield.lev) > delvort.limit
+                if (sqrt((curfield.lev[1].x- surf.bnd_x[div(surf.ndiv,2)])^2 + (curfield.lev[1].z- surf.bnd_z[div(surf.ndiv,2)])^2) > delvort.dist*surf.c)
+                    kelv_enf = kelv_enf + curfield.lev[1].s
+                    for i = 1:length(curfield.lev)-1
+                        curfield.lev[i] = curfield.lev[i+1]
+                    end
+                    pop!(curfield.lev)
+                end
+            end
+        end
+
         wakeroll(surf, curfield, dt)
 
         #Update kinematic terms in KinemPar2DOF
         update_kinem2DOF(surf)
-        
+
         #Calculate forces
         cl, cd, cm = calc_forces(surf)
 
-        #Using the force data, update - hddot and alphaddot 
+        #Using the force data, update - hddot and alphaddot
         calc_struct2DOF(surf, cl, cm)
-        mat[istep,:] = [t surf.kinem.alpha surf.kinem.h surf.kinem.u surf.a0[1] cl cd cm]        
+        mat[istep,:] = [t surf.kinem.alpha surf.kinem.h surf.kinem.u surf.a0[1] cl cd cm]
     end
 
     mat, surf, curfield
@@ -546,7 +568,7 @@ function drone_trajectory_problem(surf::TwoDFreeSurf, curfield::TwoDFlowField, n
     dt = dtstar*surf.c/surf.uref
     t = 0.
     kelv_enf = 0
-    
+
     #Intialise flowfield
     for istep = 1:nsteps
 
@@ -557,18 +579,18 @@ function drone_trajectory_problem(surf::TwoDFreeSurf, curfield::TwoDFlowField, n
         else
             dt = minimum([(0.015*0.2*3)/maximum([abs(surf.kinem.alphadot) abs(surf.kinem.hdot) abs(surf.kinem.u)]) 0.015]) #this is dimensional value - c/u from K cancels with dtstar
         end
-        
+
         #Udpate current time
         t = t + dt
 
         #Update kinematic parameters (based on 2DOF response)
         if (istep > 1) # Allow initial condition
-            update_kinem(surf, dt) 
+            update_kinem(surf, dt)
         end
         println(istep," ", t," ",length(curfield.tev)," ", length(curfield.lev))
 
         #Check if ground is breached (for drone crash problem)
-        if (surf.kinem.h < 0) 
+        if (surf.kinem.h < 0)
             mat, surf, curfield
             break
         end
@@ -583,7 +605,7 @@ function drone_trajectory_problem(surf::TwoDFreeSurf, curfield::TwoDFlowField, n
         #curfield.tev[length(curfield.tev)].s = secant_method(kelv, 0., -0.01)
         soln = nlsolve(not_in_place(kelv), [-0.01])
         curfield.tev[length(curfield.tev)].s = soln.zero[1]
-        
+
         #Check for LESP condition
         #Update values with converged value of shed tev
         #Update incduced velocities on airfoil
@@ -594,17 +616,17 @@ function drone_trajectory_problem(surf::TwoDFreeSurf, curfield::TwoDFlowField, n
 
         #Calculate first two fourier coefficients
         update_a0anda1(kelv.surf)
-        
+
         if surf.kinem.u == 0
             lesp = surf.a0[1]
         else
             lesp = surf.a0[1]/surf.kinem.u
         end
-        
+
         #Update adot
         update_a2a3adot(surf,dt)
 
-        
+
         #2D iteration if LESP_crit is exceeded
         if (abs(lesp)>surf.lespcrit[1])
             #Remove the previous tev
@@ -655,7 +677,7 @@ function drone_trajectory_problem(surf::TwoDFreeSurf, curfield::TwoDFlowField, n
                 pop!(curfield.lev)
             end
         end
-        
+
         #Calculate bound vortex strengths
         update_bv(surf)
 
@@ -663,22 +685,22 @@ function drone_trajectory_problem(surf::TwoDFreeSurf, curfield::TwoDFlowField, n
 
         #Update kinematic terms in KinemPar2DOF
         update_kinem2DOF(surf)
-        
+
         #Calculate forces
         cl, cd, cm = calc_forces(surf)
 
-        #Using the force data, update - hddot and alphaddot 
+        #Using the force data, update - hddot and alphaddot
         calc_moveFree(surf, cl, cd, cm, cf)
-        
+
         mat[istep,:] = [t surf.kinem.alpha surf.kinem.h surf.kinem.u surf.a0[1] cl cd cm surf.bnd_x[div(surf.ndiv,2)] surf.bnd_z[div(surf.ndiv,2)]]
         if (rem(istep,fr_freq) == 0)
             ind_fr += 1
             outfile = open("dump/field.$istep", "w")
             flowmat = zeros(1+length(curfield.tev)+length(curfield.lev)+length(curfield.extv)+length(surf.bv),4)
             flowmat[1,:] = [length(curfield.tev), length(curfield.lev), length(curfield.extv), length(surf.bv)]
-            flowmat[2:end,1] = [map(q->q.s, curfield.tev); map(q->q.s, curfield.lev); map(q->q.s, curfield.extv); map(q->q.s, surf.bv)] 
+            flowmat[2:end,1] = [map(q->q.s, curfield.tev); map(q->q.s, curfield.lev); map(q->q.s, curfield.extv); map(q->q.s, surf.bv)]
             flowmat[2:end,2] = [map(q->q.x, curfield.tev); map(q->q.x, curfield.lev); map(q->q.x, curfield.extv); map(q->q.x, surf.bv)]
-            flowmat[2:end,3] = [map(q->q.z, curfield.tev); map(q->q.z, curfield.lev); map(q->q.z, curfield.extv); map(q->q.z, surf.bv)] 
+            flowmat[2:end,3] = [map(q->q.z, curfield.tev); map(q->q.z, curfield.lev); map(q->q.z, curfield.extv); map(q->q.z, surf.bv)]
             writedlm(outfile, flowmat)
             close(outfile)
         end

34cc6cdb6138016fea931e226ae68d6a2148ce53
Kiran Ramesh <kiran.ramesh@glasgow.ac.uk>
2016-11-13 13:51:49 +0000

    confirm

--- src/postprocess.jl
+++ src/postprocess.jl
@@ -203,6 +203,41 @@ function calc_forces(surf::TwoDSurf_2DOF)
     return cl, cd, cm
 end
 
+
+function calc_forces_E(surf::TwoDSurf_2DOF, lev :: Float64, dt :: Float64)
+
+    # First term in eqn (2.30) Ramesh et al. in coefficient form
+    cnc = 2*pi*(surf.kinem.u*cos(surf.kinem.alpha)/surf.uref + surf.kinem.hdot*sin(surf.kinem.alpha)/surf.uref)*(surf.a0[1] + surf.aterm[1]/2.)
+
+    # Second term in eqn (2.30) Ramesh et al. in coefficient form
+    cnnc = 2*pi*(3*surf.c*surf.a0dot[1]/(4*surf.uref) + surf.c*surf.adot[1]/(4*surf.uref) + surf.c*surf.adot[2]/(8*surf.uref)) + (2*lev/(dt*surf.uref*surf.uref)) 
+
+    # Suction force given in eqn (2.31) Ramesh et al.
+    cs = 2*pi*surf.a0[1]*surf.a0[1]
+
+    #The components of normal force and moment from induced velocities are calulcated in dimensional units and nondimensionalized later
+    nonl=0
+    nonl_m=0
+    for ib = 1:surf.ndiv-1
+        nonl = nonl + (surf.uind[ib]*cos(surf.kinem.alpha) - surf.wind[ib]*sin(surf.kinem.alpha))*surf.bv[ib].s
+        nonl_m = nonl_m + (surf.uind[ib]*cos(surf.kinem.alpha) - surf.wind[ib]*sin(surf.kinem.alpha))*surf.x[ib]*surf.bv[ib].s
+    end
+    nonl = nonl*2./(surf.uref*surf.uref*surf.c)
+    nonl_m = nonl_m*2./(surf.uref*surf.uref*surf.c*surf.c)
+
+    # Normal force coefficient
+    cn = cnc + cnnc + nonl
+
+    # Lift and drag coefficients
+    cl = cn*cos(surf.kinem.alpha) + cs*sin(surf.kinem.alpha)
+    cd = cn*sin(surf.kinem.alpha)-cs*cos(surf.kinem.alpha)
+
+    #Pitching moment is clockwise or nose up positive
+    cm = cn*surf.pvt - 2*pi*((surf.kinem.u*cos(surf.kinem.alpha)/surf.uref + surf.kinem.hdot*sin(surf.kinem.alpha)/surf.uref)*(surf.a0[1]/4. + surf.aterm[1]/4. - surf.aterm[2]/8.) + (surf.c/surf.uref)*(7.*surf.a0dot[1]/16. + 3.*surf.adot[1]/16. + surf.adot[2]/16. - surf.adot[3]/64.)) - nonl_m - lev*(2*surf.pvt - 1)/(dt*surf.uref*surf.uref)
+    return cl, cd, cm
+end
+
+
 function calc_forces(surf::TwoDFreeSurf)
 
     # First term in eqn (2.30) Ramesh et al. in coefficient form

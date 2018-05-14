18944ab728accd7f4cc1cec9775cb8f64b1978e6
KiranUofG <Kiran.Ramesh@glasgow.ac.uk>
2018-05-14 22:07:46 +0100

    ibl

--- tutorials/cylinderBL/simRun.jl
+++ tutorials/cylinderBL/simRun.jl
@@ -1,11 +1,20 @@
-function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-3, writflag = 1, writeInterval = 0.5)
+#push!(LOAD_PATH,"../../src/")
+#using UNSflow
+
+include("../../src/blLagrangian/calcs.jl")
+include("../../src/blLagrangian/solvers.jl")
+include("../../src/blLagrangian/postprocess.jl")
+
+dtstar = 0.001; tTot = 3.4; m=200; n=60; tol=1e-6
+dels=1e-3; writeflag = 1; writeInterval = 0.2; maxwrite=100; nround=6
 
     # if writeflag is on, determine the timesteps to write at
     if writeflag == 1
         writeArray = Int64[]
+
         for i = 1:maxwrite
             tcur = writeInterval*real(i)
-            if t > tTot
+            if tcur > tTot
                 break
             else
                 push!(writeArray, Int(round(tcur/dtstar)))
@@ -16,14 +25,21 @@ function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-
     #Define domain in eta and eps
     del_eps = pi/m
     del_eta = 1./n
-    eps = linspace(0., pi, m+1)
-    eta = linspace(0, 1., n+1)
+    eps = [linspace(0., pi, m+1);]
+    eta = [linspace(0, 1., n+1);]
 
-    x = u = util = x_prev = u_prev = u_previter = newt = zeros(m+1, n+1)
-    z = gam = zeros(n+1)
+    x = zeros(m+1, n+1)
+    u = zeros(m+1, n+1)
+    util = zeros(m+1, n+1)
+    x_prev = zeros(m+1, n+1)
+    u_prev = zeros(m+1, n+1)
+    u_previter = zeros(m+1, n+1)
+    newt = zeros(m+1, n+1)
+    z = zeros(n+1)
+    gam = zeros(n+1)
 
     #Assign initial conditions
-    del_t = 0.001
+    del_t = dtstar
 
     for i = 1:m+1
         x[i,1:n+1] = eps[i]
@@ -44,7 +60,9 @@ function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-
     p = q = r = s = t = d = zeros(m+1, n+1)
 
     #Start time loop
-    for tt = 1:100#3200
+    istep = 0
+    while tim < tTot
+        istep += 1
         tim = tim + del_t
 
         #Store values of u and x from previous time step
@@ -67,14 +85,14 @@ function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-
             util[1,2:n] = d[1,2:n]
             util[m+1,2:n] = d[m+1,2:n]
             u[1:m+1,1] = 0.
-            u[1:m+1,n+1] = sin(x[1:m+1,n+1])
+            u[1:m+1,n+1] = sin.(x[1:m+1,n+1])
 
             util = calc_triag_util(r, q, d, util, del_eps, del_eta, del_t, m, n)
 
             #Store values of u from previous iteration
             u_previter=u
 
-            u = calc_triag_u(p, t, d, util, u, del_eps, del_eta, m, n)
+            u = calc_triag_u(p, t, d, util, u, del_eps, del_eta, del_t, m, n)
 
             #Update x
             for i = 1:m+1
@@ -84,7 +102,7 @@ function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-
             end
 
             #Check for convergence
-            newt[:,:] = 0.
+            newt[:,:] = 0
             for i = 2:m
                 for j = 2:n
                     if u[i,j] != 0
@@ -100,20 +118,22 @@ function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-
             if istep in writeArray
                 dirname = "$(round(tim,nround))"
 
-                norm = vort = y = zeros(m,n)
+                norm = zeros(m,n)
+                vort = zeros(m,n)
+                y = zeros(m,n)
                 tau = zeros(m)
 
                 y = calc_y(x, eps, eta, del_eps, del_eta, del_t, m, n)
                 tau = calc_shear(u, del_eta, m)
-                norm, minnorm = calc_minnorm(c, del_eps, del_eta, m, n)
+                norm, minnorm = calc_minnorm(x, del_eps, del_eta, m, n)
                 println(tim, minnorm)
                 vort = calc_vort(x, u, del_eps, del_eta, m, n)
 
                 writeStampBl(dirname, x, u, y, tau, norm, vort)
-            else
-                _,minnorm = calc_minnorm(c, del_eps, del_eta, m, n)
-                println(tim, minnorm)
             end
+        else
+            _,minnorm = calc_minnorm(x, del_eps, del_eta, m, n)
+            println(tim, "    ", minnorm)
         end
 
         #Check for separation (both gradients equal zero)
@@ -122,10 +142,30 @@ function blLagrangian(tTot = 1.0, m=200, n=60, tol=1e-6, write_freq=20, dels=1e-
                 del_x_eps=0.5*(x[i+1,j] - x[i-1,j])/del_eps
                 del_x_eta=0.5*(x[i,j+1] - x[i,j-1])/del_eta
                 if del_x_eps<tol && del_x_eta<tol
-                    println("singularity (separation) detected. time = $tim, i=$i, j=$j, eps=$(eps[i]), eta=$(eta[j]), x=$(x[i,j])")
+                    println("singularity (separation) detected. time = $tim,
+                        i=$i, j=$j, eps=$(eps[i]), eta=$(eta[j]), x=$(x[i,j])")
                     break
                 end
             end
         end
     end
+
+epsmat = zeros(n+1,m+1)
+etamat = zeros(n+1,m+1)
+for i = 1:m+1
+    epsmat[:,i] = eps[i]
 end
+for i = 1:n+1
+    etamat[i,:] = eta[i]
+end
+
+#blLagrangian()
+
+#writeflag = 1
+
+#writeInterval = t_tot/18.
+
+
+#makeVortPlots2D()
+
+#cleanWrite()
